638eadd09fc808b38e6357c0e99d3d6ef3dc868a
arguiot <arguiot@gmail.com>
2018-04-13 11:24:50 +0800

    Initial Commit

---
+++ CrypTools WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CrypTools WatchKit Extension
+//
+//  Created by Arthur Guiot on 13/04/2018.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ CrypTools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrypTools
+//
+//  Created by Arthur Guiot on 13/04/2018.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
