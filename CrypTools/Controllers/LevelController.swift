fe0f8e5067378a57153b49995fb8b35793318f10
arguiot <arguiot@gmail.com>
2018-04-14 21:16:34 +0800

    Changed behavior with keyboard

--- CrypTools/Controllers/LevelController.swift
+++ CrypTools/Controllers/LevelController.swift
@@ -37,6 +37,9 @@ class LevelController: UIViewController, WCSessionDelegate {
         // Do any additional setup after loading the view.
 		
 		renderMarkDown()
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -95,6 +98,22 @@ class LevelController: UIViewController, WCSessionDelegate {
 				} catch let error as NSError {
 					print(error.description)
 				}
+			}
+		}
+	}
+	
+	@objc func keyboardWillShow(notification: NSNotification) {
+		if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+			if self.view.frame.origin.y == 0{
+				self.view.frame.origin.y -= keyboardSize.height
+			}
+		}
+	}
+	
+	@objc func keyboardWillHide(notification: NSNotification) {
+		if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+			if self.view.frame.origin.y != 0{
+				self.view.frame.origin.y += keyboardSize.height
 			}
 		}
 	}
